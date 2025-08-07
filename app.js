d1b6ae1d49adb636f39f1b039afca3a2af9c6ad7
Ahmed raza <ahmed.raza1054@gmail.com>
2025-08-08 03:00:14 +0500

    working on the logic

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
-let url = "http://universities.hipolabs.com/search?country=pakistan";
+let url = "http://universities.hipolabs.com/search";
+let dropDown = document.querySelector("#dropDown");
 
 let getUnilist = async () => {
   try {
     let res = await fetch(url);
-    let data = await res.json();
-    console.log(data);
+    let resData = await res.json();
+
+    let addedCountries = new Set();
+
+    for (let data of resData) {
+      if (!addedCountries.has(data.country)) {
+        let option = document.createElement("option");
+        option.innerText = data.country;
+        option.value = data.country;
+        dropDown.appendChild(option);
+        addedCountries.add(data.country);
+      }
+    }
   } catch (error) {
     console.log("Error -->", error);
   }
 };
+
+// getUnilist();
